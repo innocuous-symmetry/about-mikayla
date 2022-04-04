786d8e3d3510701176bf1823625e1fc4202a05e3
Mikayla Dobson <93477693+innocuous-symmetry@users.noreply.github.com>
2022-04-04 17:19:15 -0500

    new deps

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,4 +1,5 @@
 import '../App.scss';
+import { useEffect, useState } from 'react';
 
 import Paper from '@mui/material/Paper';
 import Avatar from '@mui/material/Avatar';
@@ -14,32 +15,48 @@ const { themeA } = colorThemes;
 
 
 export default function Welcome() {
+    const [gallery, setGallery] = useState([0,1]);
+
+    const galleryButtons = [
+        <Button variant="contained" href="/projects" sx={buttonStyle}>What kinds of things do I do?</Button>,
+        <Button variant="contained" href="/technologies" sx={buttonStyle}>What do I use to do it?</Button>,
+        <Button variant="contained" href="/in-progress" sx={buttonStyle}>What projects am I working on now?</Button>,
+        <Button variant="contained" href="/links" sx={buttonStyle}>Where can you find more about me and my work?</Button>,
+        <Button variant="contained" href="/creative-works" sx={buttonStyle}>Where can I listen to some of your music?</Button>
+    ]
+
+    // handle gallery debug
+    useEffect(() => {
+        console.log(gallery);
+    }, [gallery]);
+
+    const handleGallery = () => {
+        let newState = [];
+        for (let each of gallery) {
+            let newNum = (each + 1) % galleryButtons.length;
+            newState.push(newNum);
+        }
+        setGallery(newState);
+    }
+
     return (
         <div style={pageTheme}>
             <div className="landing">
                 <Avatar alt="Mikayla Dobson" src={profile} sx={{width: 110, height: 110}} />
                 <h3>Hi, my name is Mikayla! I'm a junior-level full stack web developer.</h3>
-                <p>I excel in building well-structured and maintainable web applications, managing algorithmic complexity, and adapting my workflow to fit the needs of any environment I should find myself in.</p>
+                <p>I excel in building well-structured and maintainable web applications, managing algorithmic complexity,
+                    and adapting my workflow to fit the needs of any environment I should find myself in.
+                </p>
             </div>
 
             <h3 className="do-stuff">Thanks for visiting! Feel free to peruse below:</h3>
             
             <div style={galleryTheme} className="gallery">
-                <Button variant="contained" href="/projects" sx={buttonStyle}>What kinds of things do I do?</Button>
-
-                <Paper elevation={5} sx={[themeA, cardTheme]}>
-                    <Button href="/technologies" sx={linkStyle}>What do I use to do it?</Button>
-                </Paper>
-
-                <Paper elevation={5} sx={[themeA, cardTheme]}>
-                    <Button href="/in-progress" sx={linkStyle}>What projects am I working on now?</Button>
-                </Paper>
-
-                <Paper elevation={5} sx={[themeA, cardTheme]}>
-                    <Button href="/links" sx={linkStyle}>Where can you find more about me and my work?</Button>
-                </Paper>
-
+                {galleryButtons[gallery[0]]}
+                {galleryButtons[gallery[1]]}
             </div>
+
+            <Button variant="contained" onClick={handleGallery}>Gallery?</Button>
         </div>
     )
 }
--- src/styles/Style.js
+++ src/styles/Style.js
@@ -1,43 +1,42 @@
 import { red, green, pink, deepPurple, purple } from '@mui/material/colors';
-import { makeStyles } from '@mui/material';
 
-export const useStyles = makeStyles({
-    colorThemeA: {
-        backgroundColor: pink[50],
-        color: deepPurple[300]
-    },
-    html: {
-        backgroundColor: purple[200],
-    },
-    muiButton: {
-        backgroundColor: deepPurple[200],
-        color: deepPurple[500],
-        '&:hover': {
-            backgroundColor: green[400],
-        }
-    },
-    welcomePage: {
-        display: 'flex',
-        backgroundColor: purple[100],
-        height: '100vh',
-        flexDirection: 'column',
-        alignItems: 'center',
-        overflowX: 'hidden'
-    },
-    gallery: {
-        display: 'flex',
-        width: '90vw',
-        justifyContent: 'center',
-    },
-    card: {
-        width: '250px',
-        height: '250px',
-        margin: '2rem',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',  
-    }
-});
+// export const useStyles = ({
+//     colorThemeA: {
+//         backgroundColor: pink[50],
+//         color: deepPurple[300]
+//     },
+//     html: {
+//         backgroundColor: purple[200],
+//     },
+//     muiButtonA: {
+//         backgroundColor: deepPurple[200],
+//         color: deepPurple[500],
+//         '&:hover': {
+//             backgroundColor: purple[400],
+//         }
+//     },
+//     welcomePage: {
+//         display: 'flex',
+//         backgroundColor: purple[100],
+//         height: '100vh',
+//         flexDirection: 'column',
+//         alignItems: 'center',
+//         overflowX: 'hidden'
+//     },
+//     gallery: {
+//         display: 'flex',
+//         width: '90vw',
+//         justifyContent: 'center',
+//     },
+//     card: {
+//         width: '250px',
+//         height: '250px',
+//         margin: '2rem',
+//         display: 'flex',
+//         alignItems: 'center',
+//         justifyContent: 'center',  
+//     }
+// });
 
 export const DocumentStyle = {
     colorThemes: {
@@ -57,7 +56,7 @@ export const DocumentStyle = {
         backgroundColor: deepPurple[200],
         color: deepPurple[500],
         '&:hover': {
-            backgroundColor: green[400],
+            backgroundColor: purple[400],
         }
     }
 }
